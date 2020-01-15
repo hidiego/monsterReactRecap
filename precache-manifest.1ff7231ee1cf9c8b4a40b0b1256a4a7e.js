0a50ff72d390cd03f2ec71f30cca24c44e7be833
dcorral <dcorral@minsait.com>
2020-01-16 00:15:32 +0100

    Updates

---
+++ precache-manifest.1ff7231ee1cf9c8b4a40b0b1256a4a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731a83ff13b4fb3d1adfff5e470b7266",
+    "url": "/monsterReactRecap/index.html"
+  },
+  {
+    "revision": "fbd9a2756258b0321a93",
+    "url": "/monsterReactRecap/static/css/main.6e1f10f1.chunk.css"
+  },
+  {
+    "revision": "8891ad6c2c9c48dde409",
+    "url": "/monsterReactRecap/static/js/2.46aa02fd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsterReactRecap/static/js/2.46aa02fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbd9a2756258b0321a93",
+    "url": "/monsterReactRecap/static/js/main.96aec193.chunk.js"
+  },
+  {
+    "revision": "0cc9f25d71d334c0274e",
+    "url": "/monsterReactRecap/static/js/runtime-main.242c89e9.js"
+  }
+]);
